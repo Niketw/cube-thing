04c2ed53ae54223fd4f4cc37e1a8b4e97d7efa1b
Aniket Inamdar <asinamdar2024@gmail.com>
2024-10-12 16:06:48 +0530

    fixed things

--- src/renderer/MainFrame.java
+++ src/renderer/MainFrame.java
@@ -7,18 +7,10 @@ import java.io.File;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
-        super("Cycles 3D Renderer");
+        super("cube thing");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(new Renderer());
         setSize(800, 600);
-
-        try {
-            Image icon = ImageIO.read(new File("./src/assets/icon.png"));
-            setIconImage(icon);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         setLocationRelativeTo(null);
         setVisible(true);
     }
